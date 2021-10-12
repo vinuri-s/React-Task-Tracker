9713cc586c7d87065dee389c98d847d04282cf10
ManulMax <manulmadara9725@gmail.com>
2021-10-12 20:54:55 +0530

    Add cancel close func

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({title,onAdd,showAdd}) => {
+const Header = ({title,onAdd,showAdd,onOff}) => {
 
     const onClick = () =>{
         console.log('click');
     }
+
+    function onOff() {
+        var ww = window.open(window.location, '_self'); ww.close();
+      }
 
     return (
         <header className='header'>
@@ -13,7 +17,7 @@ const Header = ({title,onAdd,showAdd}) => {
                 {title}
             </h1>
             <Button color={showAdd ?'red':'green'} text={showAdd ? 'Close' :'Add'} onClick={onAdd}/>
-            <Button color={'red'} text={'Cancel'} />
+            <Button color={'red'} text={'Cancel'} onClick={onOff}/>
         </header>
     )
 }
